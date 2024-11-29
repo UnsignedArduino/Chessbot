aeee341fd255c2ad121770c405ba1f5ae620a1b3
Cyrus Yiu <UnsignedArduino@outlook.com>
2024-11-29 16:53:46 -0500

    NCNN

--- src/train/test_board_segmentation.py
+++ src/train/test_board_segmentation.py
@@ -5,8 +5,13 @@ from picamera2 import Picamera2
 from ultralytics import YOLO
 
 model_path = Path.cwd() / "src" / "models" / "board_segmentation_best.pt"
+ncnn_path = Path.cwd() / "src" / "models" / "board_segmentation_best_ncnn_model"
 
-model = YOLO(model_path)
+if not ncnn_path.exists():
+    model = YOLO(model_path)
+    model.export(format="ncnn")
+
+model = YOLO(ncnn_path, task="segment")
 
 cam = Picamera2()
 cam.configure(
@@ -15,7 +20,7 @@ cam.start()
 
 while True:
     frame = cam.capture_array()
-    results = model(frame)
+    results = model(frame, verbose=False)
     annotated = results[0].plot()
 
     cv2.imshow("Board segmentation inference", annotated)
