4807bb443e4d05b0d17354fc688220de9dad1128
Cyrus Yiu <UnsignedArduino@outlook.com>
2024-12-23 21:21:40 -0500

    Automatic "slideshow" option to reduce keyboard pressing

--- src/main.py
+++ src/main.py
@@ -19,17 +19,25 @@ parser = ArgumentParser(description="A Raspberry Pi that uses a camera to look a
                                     "board and tells you it's move on a monitor.")
 parser.add_argument("--debug-use-image-dir", type=Path, default=None,
                     help="Use a directory of images instead of the camera as the "
-                         "source")
+                         "source.")
+parser.add_argument("--debug-play-image-dir", action="store_true",
+                    help="Play images in the specified directory instead of waiting "
+                         "for key presses to cycle through. (like a slideshow)")
 parser.add_argument("-v", "--verbose", action="store_true",
-                    help="Enable verbose logging")
+                    help="Enable verbose logging.")
 args = parser.parse_args()
 logger.debug(args)
 if args.verbose:
     logger.debug("Enabling verbose logging")
     set_all_stdout_logger_levels(logging.DEBUG)
 
+debug_play_image_dir = bool(args.debug_play_image_dir)
+
 cam = None
 debug_image_dir = Path(args.debug_use_image_dir) if args.debug_use_image_dir else None
+
+if debug_play_image_dir and debug_image_dir is None:
+    raise ValueError("You must specify a directory of images to play.")
 
 if debug_image_dir is not None:
     logger.info(f"Using image directory {debug_image_dir} for debugging")
@@ -67,18 +75,44 @@ while True:
     key = ""
     if debug_image_dir is not None:
         logger.info("Use 'a' or 's' to go back, 'd' or 'w' to go forward, 'q' to quit.")
-        while True:
-            key = chr(cv2.waitKey(0) & 0xFF)
+        if debug_play_image_dir:
+            logger.info("Playing images in directory, automatically moving to the "
+                        "next one. If a key specified above is pressed, automatic "
+                        "playback will stop.")
+            key = chr(cv2.waitKey(1) & 0xFF)
+            pressed_key = False
             if key in ("d", "w"):
                 cam.image_index += 1
+                pressed_key = True
             elif key in ("a", "s"):
                 cam.image_index -= 1
+                pressed_key = True
             elif key == "q":
+                pressed_key = True
                 pass
             else:
-                print("Unknown key")
-                continue
-            break
+                if cam.image_index < cam.max_image_index:
+                    logger.debug("Automatically moving to next image now")
+                    cam.image_index += 1
+                else:
+                    logger.debug("Reached end, stopping")
+                    debug_play_image_dir = False
+            if pressed_key:
+                logger.info("Key pressed, stopping automatic playback")
+                debug_play_image_dir = False
+        else:
+            while True:
+                key = chr(cv2.waitKey(0) & 0xFF)
+                if key in ("d", "w"):
+                    cam.image_index += 1
+                elif key in ("a", "s"):
+                    cam.image_index -= 1
+                elif key == "q":
+                    pass
+                else:
+                    print("Unknown key")
+                    continue
+                break
     else:
         key = chr(cv2.waitKey(1) & 0xFF)
     if key == "q":
